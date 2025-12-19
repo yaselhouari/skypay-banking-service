322980cb7d9ad33e80ad78ce3f77b4071cfbce01
yassine <yas.elhouari@gmail.com>
2025-12-19 15:30:24 +0100

    chore: add main class for manual testing

--- src/main/java/ma/skypay/bankingservice/Main.java
+++ src/main/java/ma/skypay/bankingservice/Main.java
@@ -1,9 +1,17 @@
 package ma.skypay.bankingservice;
 
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Banking Service");
+        Account account = new Account();
+
+        account.deposit(1000, LocalDate.of(2012, 1, 10));
+        account.deposit(2000, LocalDate.of(2012, 1, 13));
+        account.widthdraw(500, LocalDate.of(2012, 1, 14));
+
+        account.printStatement();
     }
 }
